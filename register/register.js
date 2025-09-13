78bfd8e12c34b4046650d4594cff44818cf6c0bd
Baybhin Gurung <baybhin@gmail.com>
2025-09-13 14:59:35 -0400

    Completed Jira tasks and look at notes

---
+++ register/register.js
@@ -0,0 +1,10 @@
+// Register.html methods
+
+const register = async () => {
+    console.log("Register button logic goes here...")
+}
+
+const loadTopWensites = async () => {
+    console.log("Load top 5 sites logic goes here...")
+}
+
