f8e40a18e4500bf3d478e72534c63fda0a32b4ac
Baybhin Gurung <baybhin@gmail.com>
2025-09-15 17:40:53 -0400

    Remove these files

--- learn/learn.js
+++
@@ -1,9 +0,0 @@
-// Learn.js codes
-
-const loadPlayStatus = async () => {
-    console.log("Load player status button logic here...")
-}
-
-const levelUp = async () => {
-    console.log("Level up logic goes here...")
-}
