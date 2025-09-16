364bfbce16b3928ff70b0aef506031d1a0bc2776
Baybhin Gurung <baybhin@gmail.com>
2025-09-15 22:11:02 -0400

    Add logic for clicking on lessons

--- learn/learn.js
+++ learn/learn.js
@@ -34,9 +34,28 @@ const subjectButtonLogic = (event) => {
     }
 }
 
+// 3 - Lesson button functionalities (dummy function)
+
+const lessonButtonLogic = (event) => {
+
+    const lessonActions = {}
+    for(let i = 1; i < 11; i++) {
+        lessonActions[`Lesson ${i}`] = () => console.log(`Lesson ${i} button clicked`);
+    }
+
+    // Show the modal
+    const action = lessonActions[event.target.innerText]
+    if(action) {
+        action();
+    } else {
+        console.log("Unknown lesson clicked");
+    }
+}
+
 // When all the HTML loads
 document.addEventListener('DOMContentLoaded', function() {
-    // Find the button and attach the function
+
+
     document.querySelectorAll('.category-btn').forEach(button => {
         button.addEventListener('click', function(event) {
             categoriesButtonLogic(event);
@@ -46,6 +65,12 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('.subject-btn').forEach(button => {
         button.addEventListener('click', function(event) {
             subjectButtonLogic(event);
+        });
+    });
+
+    document.querySelectorAll('.unit-btn').forEach(button => {
+        button.addEventListener('click', function(event) {
+            lessonButtonLogic(event);
         });
     });
 });
